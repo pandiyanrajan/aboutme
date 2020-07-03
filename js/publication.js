c64a799f6073dc89febb25fd450ddbea528e4d0d
pandiyanrajan <pandiyan.rajan8@gmail.com>
2020-07-03 21:40:48 +0530

    Conference card details added

--- js/publication.js
+++ js/publication.js
@@ -11,39 +11,84 @@ var publicationContent = [{
 		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
 	},
 	{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
-		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"national_international" : "International",
+		"title_of_the_paper" : "Effect Of Zro2 And Graphite Particulates On Mechanical Behavior Of Aluminum Matrix Composite",
+		"name_of_the_conference" : "International Conference on Contemporary Design and Analysis of Manufacturing and Industrial Engineering systems (CDAMIES 2018) ",
+		"organizing_institute" : "National Institute of Technology, Tiruchirappa lli,",
+		"date" : "20.01.2018",
+		"area_of_publication" : "Materials",
+		"co_authour" : "P. Maran , S. Marimuthu"
 	},
 	{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
-		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"national_international" : "International",
+		"title_of_the_paper" : "Numerical Analysis of Line Contact Fatigue in rail Wheels During Rolling Action",
+		"name_of_the_conference" : "International conference on evolving trends in engineering and technology",
+		"organizing_institute" : "Latha Mathavan engineering college- Madurai",
+		"date" : "28.05.2015",
+		"area_of_publication" : "FEA",
+		"co_authour" : " -- "
 	},
 	{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
-		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"national_international" : "International",
+		"title_of_the_paper" : "Study of aluminum Matrix Composite",
+		"name_of_the_conference" : "International conference on modeling, Simulation, and control ",
+		"organizing_institute" : "Karpagam college of engineering Coimbatore",
+		"date" : "15 and 16 October 2015",
+		"area_of_publication" : "Materials",
+		"co_authour" : " -- "
+	},
+	{
+		"national_international" : "International",
+		"title_of_the_paper" : "Parameter Optimization of welded joints by using GA and NNA Method",
+		"name_of_the_conference" : "International conference on advances in industrial engineering application",
+		"organizing_institute" : "Anna University Chennai",
+		"date" : "6 August 2014",
+		"area_of_publication" : "Materials",
+		"co_authour" : " -- "
+	},
+	{
+		"national_international" : "International",
+		"title_of_the_paper" : "Improving mechanical strength on welded joints by using optimization technique",
+		"name_of_the_conference" : "International conference on modeling, optimization and computing",
+		"organizing_institute" : "Noorul Islam university kumaracoil ",
+		"date" : "10 and 11 April 2014",
+		"area_of_publication" : "Materials",
+		"co_authour" : " -- "
+	},
+	{
+		"national_international" : "International",
+		"title_of_the_paper" : "Contact stress Distribution of large diameter ball bearing using Hertzian Elliptical contact theory",
+		"name_of_the_conference" : "International conference on modeling optimization and computing",
+		"organizing_institute" : "Noorul Islam university kumaracoil",
+		"date" : "10 and 11 April 2012",
+		"area_of_publication" : "FEA",
+		"co_authour" : " -- "
+	},
+	{
+		"national_international" : "International",
+		"title_of_the_paper" : "Analytical and numerical determination of contact stress in large diameter ball bearing using Hertzian elliptical contact theory.",
+		"name_of_the_conference" : "IEEE- international conference on advance in engineering, science and management",
+		"organizing_institute" : "E.G.S PILLAY Engineering College- Nagapattinam",
+		"date" : "30 and 31 March 2012",
+		"area_of_publication" : "FEA",
+		"co_authour" : " -- "
+	},
+	{
+		"national_international" : "International",
+		"title_of_the_paper" : "Fatigue Crack Growth in High Strength steel under contact cyclic loading.",
+		"name_of_the_conference" : "Emerging trends in mechanical engineering (ETME 2012)",
+		"organizing_institute" : "KLN College of Engineering, Madurai.",
+		"date" : "02 and 03 April 2012",
+		"area_of_publication" : "FEA",
+		"co_authour" : " -- "
 	}]
 },
 {
-	"title"  : "Conference",
+	"title"  : "Journals",
 	"content" : [{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
+		"national_international" : "International",
+		"title_of_the_paper" : "Mechanical and Metallurgical Characterization Of Friction Stir Welded AA6061- ZRO2-C Hybrid MMCs",
+		"name_of_the_conference" : "Materials Today: Proceedings, Elsevier",
 		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
 		"date" : "22.03.2019",
 		"area_of_publication" : "Materials ",
