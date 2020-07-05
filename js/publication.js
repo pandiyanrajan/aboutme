f7a05e969721708dbba2ff1f1ff2a507dc1ee0c4
pandiyanrajan <pandiyan.rajan8@gmail.com>
2020-07-05 13:49:27 +0530

    Publication tabs over with Conference, journal and published part

--- js/publication.js
+++ js/publication.js
@@ -1,6 +1,6 @@
 
-var publicationContent = [{
-	"title"  : "Conference",
+var publicationContent = {
+	"title"  : "Conference - Research Publications",
 	"content" : [{
 		"national_international" : "National",
 		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
@@ -82,81 +82,209 @@ var publicationContent = [{
 		"area_of_publication" : "FEA",
 		"co_authour" : " -- "
 	}]
-},
-{
-	"title"  : "Journals",
+};
+var journalContent = {
+	"title"  : "Journals Research Publications ",
 	"content" : [{
 		"national_international" : "International",
 		"title_of_the_paper" : "Mechanical and Metallurgical Characterization Of Friction Stir Welded AA6061- ZRO2-C Hybrid MMCs",
-		"name_of_the_conference" : "Materials Today: Proceedings, Elsevier",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
+		"name_of_the_journal" : "Materials Today: Proceedings, Elsevier",
+		"details" : "ISSN(online22147853",
 		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"co_authour" : "Maran.P,  K.Arumugam ,Marimuthu.S"
 	},
 	{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
-		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"national_international" : "International ",
+		"title_of_the_paper" : "Improving Mechanical Strength on Welded Joints By Using Optimization Technique",
+		"name_of_the_journal" : "International Journal of Enterprise Network Management ",
+		"details" : "ISSN online 1748-1260 , ISSN print 1748-1252",
+		"area_of_publication" : "Optimization",
+		"co_authour" : " -- "
 	},
 	{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
-		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"national_international" : "International ",
+		"title_of_the_paper" : "Friction stir welding of hybrid AA 6061-ZrO2-C composites FSW process optimization using desirability approach",
+		"name_of_the_journal" : "Materials Research Express",
+		"details" : "ISSN: 2053-1591",
+		"area_of_publication" : "Optimization ",
+		"co_authour" : "P Maran,  NMurugan,  S Marimuthu and  T Sornakumar"
 	},
 	{
-		"national_international" : "National",
-		"title_of_the_paper" : "Tribological behavior of aluminum matrix (aa6161), zro2 and c reinforced hybrid composite materials",
-		"name_of_the_conference" : "National Conference on Recent Advances in Manufacturing, Design and Thermal Engineering (RAMDTE 2019)",
-		"organizing_institute" : "Sri Ramakrishn a Institute of Technology, Coimbatore",
-		"date" : "22.03.2019",
+		"national_international" : "International ",
+		"title_of_the_paper" : "Mechanical and tribological behavior of the metal matrix compositeAA6061/ZrO2/C",
+		"name_of_the_journal" : "Journal of mechanical science and technology",
+		"details" : "ISSN: 1738-494X (print version) ISSN: 1976-3824 (electronic version)",
 		"area_of_publication" : "Materials ",
-		"co_authour" : "r.venkatesan radha krishnan p.muthu pandi m.pal muthu"
+		"co_authour" : "Marn.P,  Marimuthu,S,  Ganesh KC"
+	},
+	{
+		"national_international" : "National  ",
+		"title_of_the_paper" : "Synthesis and Characterization of Zirconium Dioxide Particulate Reinforced Aluminum Alloy Metal Matrix Composite",
+		"name_of_the_journal" : "Indian Journal of Engineering & Materials Sciences",
+		"details" : "ISSN: 0975-1017 (Online); 09714588 (Print)",
+		"area_of_publication" : "Materials ",
+		"co_authour" : "Marn.P,  Marimuthu, S, Ganesh"
+	},
+	{
+		"national_international" : "International ",
+		"title_of_the_paper" : "Correlation of fracture parameters during onset of crack in middle tension specimen",
+		"name_of_the_journal" : " Journal of Computational Design and Engineering-Elsevier ",
+		"details" : "ISSN: 2288-4300",
+		"area_of_publication" : "FEA",
+		"co_authour" : "Starvin MS,  Ganesh KC"
+	},
+	{
+		"national_international" : "International ",
+		"title_of_the_paper" : "Contact Stress Distribution of Large Diameter Ball Bearing Using Hertzian Elliptical Contact Theory",
+		"name_of_the_journal" : "Procedia Engineering- Elsevier",
+		"details" : "ISSN: 1877-7058",
+		"area_of_publication" : "FEA",
+		"co_authour" : "Starvin MS,  Ganesh KC"
+	},
+	{
+		"national_international" : "International ",
+		"title_of_the_paper" : "Verification  And Study Of Line Contact Stress In Rail Wheel Application",
+		"name_of_the_journal" : "International Journal of Applied Engineering Research",
+		"details" : "ISSN 0973-4562",
+		"area_of_publication" : "FEA",
+		"co_authour" : "maheswaran. k.dwaragakannan., n.garavind"
 	}]
-}];
+};
 
-function loadCard()
+var publishedContent = {
+	"title" : "Monographs Developed/ Books Published",
+	"content" : [{
+		"title" : "DYNAMICS OF MACHINE",
+		"co_authour" : "R.PANDIYARAJAN",
+		"publisher" : " -- ",
+		"year" : "2019"
+	},
+	{
+		"title" : "WELDING TECHNOLOGY",
+		"co_authour" : "R.PANDIYARAJAN",
+		"publisher" : " -- ",
+		"year" : "2019"
+	},
+	{
+		"title" : "KINEMATICS OF MACHINE",
+		"co_authour" : "R.PANDIYARAJAN",
+		"publisher" : " -- ",
+		"year" : "2018"
+	}]
+};
+
+function loadJornal()
 {
 	var html = "";
-	for (var i = 0; i < publicationContent.length;i++) {
 
-		html += '<br>';
-		html += '<h5 align="center">' + publicationContent[i].title + '</h5>';
-		html += "<hr>";
-		html += '<div class="mx-4">';
-		html += '<div id="publicationCard" class="row row-cols-1 row-cols-md-3">';
+	html += '<br>';
+	html += '<h5 align="center">' + journalContent.title + '</h5>';
+	html += "<hr>";
+	html += '<div class="mx-4">';
+	html += '<div id="publicationCard" class="row row-cols-1 row-cols-md-2">';
+	
+	
+	for (var j = 0; j < journalContent.content.length;j++) {
 		
-		
-		for (var j = 0; j < publicationContent[i].content.length;j++) {
-			
-			html += '<div class="col mb-4">';
-		    html += '<div class="shadow card h-100">';
-		    html += '<div class="card-body">';
-		    html += '<h6 class="card-title"> <strong>National / International : </strong>' + publicationContent[i].content[j].national_international + '</h6>';
-		    html += '<h6 class="card-title"> <strong>Date : </strong>' + publicationContent[i].content[j].date + '</h6>';
-		    html += '<h6 class="card-title"> <strong>Title of the paper : </strong>' + publicationContent[i].content[j].title_of_the_paper + '</h6>';
-		    html += '<h6 class="card-title"> <strong>Organizing institute : </strong>' + publicationContent[i].content[j].organizing_institute + '</h6>';
-		    html += '<h6 class="card-title"> <strong>Area of publication : </strong>' + publicationContent[i].content[j].area_of_publication + '</h6>';
-		    html += '<h6 class="card-title"> <strong>Co authour : </strong>' + publicationContent[i].content[j].co_authour + '</h6>';
-		    html += '<p class="card-text"> <strong>Name of the conference : </strong>' + publicationContent[i].content[j].name_of_the_conference + '</p>';
-		    html += '</div>';
-		    html += '</div>';
-		    html += '</div>';
-		}
-
-		html += '</div></div>';
+		html += '<div class="col mb-4">';
+	    html += '<div class="shadow card h-100">';
+	    html += '<div class="card-body">';
+	    html += '<h6 class="card-title"> <strong>National / International : </strong>' + journalContent.content[j].national_international + '</h6>';
+	    html += '<h6 class="card-title"> <strong>Title of the paper : </strong>' + journalContent.content[j].title_of_the_paper + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Details : </strong>' + journalContent.content[j].details + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Area of publication : </strong>' + journalContent.content[j].area_of_publication + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Co authour : </strong>' + journalContent.content[j].co_authour + '</h6>';
+	    html += '<p class="card-text"> <strong>Name of the Journal : </strong>' + journalContent.content[j].name_of_the_journal + '</p>';
+	    html += '</div>';
+	    html += '</div>';
+	    html += '</div>';
 	}
+
+	html += '</div></div>';
+	return html;
+ }
+
+function loadConference()
+{
+	var html = "";
+
+	 html += '<br>';
+	html += '<h5 align="center">' + publicationContent.title + '</h5>';
+	html += "<hr>";
+	html += '<div class="mx-4">';
+	html += '<div id="publicationCard" class="row row-cols-1 row-cols-md-2">';
+	
+	
+	for (var j = 0; j < publicationContent.content.length;j++) {
+		
+		html += '<div class="col mb-4">';
+	    html += '<div class="shadow card h-100">';
+	    html += '<div class="card-body">';
+	    html += '<h6 class="card-title"> <strong>National / International : </strong>' + publicationContent.content[j].national_international + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Date : </strong>' + publicationContent.content[j].date + '</h6>';
+	    html += '<h6 class="card-title"> <strong>Title of the paper : </strong>' + publicationContent.content[j].title_of_the_paper + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Organizing institute : </strong>' + publicationContent.content[j].organizing_institute + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Area of publication : </strong>' + publicationContent.content[j].area_of_publication + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Co authour : </strong>' + publicationContent.content[j].co_authour + '</h6>';
+	    html += '<p class="card-text"> <strong>Name of the conference : </strong>' + publicationContent.content[j].name_of_the_conference + '</p>';
+	    html += '</div>';
+	    html += '</div>';
+	    html += '</div>';
+	}
+
+	html += '</div></div>';
 	return html;
 }
 
+function loadPublished()
+{
+	var html = "";
+
+	html += '<br>';
+	html += '<h5 align="center">' + publishedContent.title + '</h5>';
+	html += "<hr>";
+	html += '<div class="mx-4">';
+	html += '<div id="publicationCard" class="row row-cols-1 row-cols-md-3">';
+	
+	
+	for (var j = 0; j < publishedContent.content.length;j++) {
+		
+		html += '<div class="col mb-4">';
+	    html += '<div class="shadow card h-100">';
+	    html += '<div class="card-body">';
+	    html += '<h6 class="card-title"> <strong>Title : </strong>' + publishedContent.content[j].title + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Year : </strong>' + publishedContent.content[j].year + '</h6>';
+	    html += '<h6 class="card-text"> <strong>Co authour : </strong>' + publishedContent.content[j].co_authour + '</h6>';
+	    html += '<h6 class="card-title"> <strong>Publisher : </strong>' + publishedContent.content[j].publisher + '</h6>';
+	    html += '</div>';
+	    html += '</div>';
+	    html += '</div>';
+	}
+
+	html += '</div></div>';
+	return html;
+}
+
+function loadTabs() {
+	
+	var html = '<br><nav align="center" class="nav nav-pills justify-content-center">';
+  	html += '<div class="nav nav-tabs" id="nav-tab" role="tablist">';
+    html += '<a class="nav-item nav-link active" id="nav-conference-tab" data-toggle="tab" href="#nav-conference" role="tab" aria-controls="nav-conference" aria-selected="true">Conference</a>';
+    html += '<a class="nav-item nav-link" id="nav-journal-tab" data-toggle="tab" href="#nav-journal" role="tab" aria-controls="nav-journal" aria-selected="false">Journal</a>';
+    html += '<a class="nav-item nav-link" id="nav-published-tab" data-toggle="tab" href="#nav-published" role="tab" aria-controls="nav-published" aria-selected="false">Published</a>';
+  	html += '</div>';
+	html += '</nav>';
+	html += '<div class="tab-content" id="nav-tabContent">';
+  	html += '<div class="tab-pane fade show active" id="nav-conference" role="tabpanel" aria-labelledby="nav-conference-tab">...</div>';
+  	html += '<div class="tab-pane fade" id="nav-journal" role="tabpanel" aria-labelledby="nav-journal-tab">...</div>';
+  	html += '<div class="tab-pane fade" id="nav-published" role="tabpanel" aria-labelledby="nav-published-tab">...</div>';
+	html += '</div>';
+
+	return html;
+}
 $(document).ready(function(){
-	$("#base").append(loadCard());
+	$("#base").append(loadTabs());
+	$("#nav-conference").html(loadConference());
+	$("#nav-journal").html(loadJornal());
+	$("#nav-published").html(loadPublished());
 });
